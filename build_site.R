e0926ca1cc9f6a1a24b87ff22d2516833fa93571
Benjamin Pontiller <BenPonBiobrain@users.noreply.github.com>
2020-09-17 10:51:28 +0200

    Delete build_site.R

--- build_site.R
+++
@@ -1,9 +0,0 @@
-#Set our working directory. 
-#This helps avoid confusion if our working directory is 
-#not our site because of other projects we were 
-#working on at the time. 
-setwd("/Users/bepoaa/projects/my_website/personal_website/")
-
-#render your sweet site. 
-rmarkdown::render_site()
-
